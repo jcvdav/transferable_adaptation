34fdbc866cd9c89caa4837ba3322e56cea030e55
jcvdav <juancarlos.villader@gmail.com>
2026-04-13 12:14:39 -0400

    Updated script to include figure exporting code

--- scripts/04_enabling_conditions_scores.R
+++ scripts/04_enabling_conditions_scores.R
@@ -134,7 +134,7 @@ country_scores <- calculate_group_score(enabling_scores, "country") %>%
 
 #Absence of X is not a limiting condition. Instead, things like "rules that say you can't do X" are limitng conditons
 
-enabling_scores %>% 
+heatmap_plot <- enabling_scores %>% 
   group_by(ar_text) %>% 
   mutate(n_cases = n()) %>% 
   ungroup() %>% 
@@ -163,7 +163,8 @@ enabling_scores %>%
   theme(axis.title = element_blank(),
         axis.text.x = element_text(angle = 45,
                                    hjust = 0))
-  
+
+heatmap_plot
 
 # X ----------------------------------------------------------------------------
 plot_scores <- function(data, group) {
@@ -216,4 +217,5 @@ enabling_scores_plot
 
 ## EXPORT ######################################################################
 
-# X ----------------------------------------------------------------------------
+ggsave(here("figures", "enabling_conditions_scores_heatmap.png"), plot = heatmap_plot, width = 10, height = 7, dpi = 300)
+ggsave(here("figures", "enabling_conditions_scores.png"), plot = enabling_scores_plot, width = 6, height = 8, dpi = 300)
